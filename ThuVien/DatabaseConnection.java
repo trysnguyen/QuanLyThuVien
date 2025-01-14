4d367df770c836688e05751c2b38b613d8f140f4
trysnguyen <122746846+trysnguyen@users.noreply.github.com>
2025-01-15 02:11:42 +0700

    Update DatabaseConnection.java

--- ThuVien/DatabaseConnection.java
+++ ThuVien/DatabaseConnection.java
@@ -10,8 +10,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/QUANLYTHUVIEN";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Banhbun@123";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         Connection connection = null;
